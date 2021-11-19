5a02d8b180d2e7b1228ec54991c7e3a2d3486976
RyoJerryYu <tzl19961216@gmail.com>
2021-11-19 21:43:38 +0800

    chore(engine): rename default engine option

--- engine/opts.go
+++ engine/opts.go
@@ -5,13 +5,13 @@ import (
 	"github.com/RyoJerryYu/gogoengine/ui"
 )
 
-func SetGame(game game.Game) engineOption {
+func WithGame(game game.Game) engineOption {
 	return func(e *engine) {
 		e.game = game
 	}
 }
 
-func SetUI(ui ui.UserInterface) engineOption {
+func WithUI(ui ui.UserInterface) engineOption {
 	return func(e *engine) {
 		e.ui = ui
 	}
