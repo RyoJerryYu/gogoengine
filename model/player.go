203fd646212aec4cac9ae7a43dbf74c42ab1c97c
RyoJerryYu <tzl19961216@gmail.com>
2021-11-20 12:59:36 +0800

    feat(player): add model code player

--- model/player.go
+++ model/player.go
@@ -2,4 +2,26 @@ package model
 
 type Player interface {
 	Passed() bool
+	GetStoneType() StoneType
+	GetTag() string
+}
+
+type player struct {
+	stoneType StoneType
+	passed    bool
+	tag       string
+}
+
+var _ Player = (*player)(nil)
+
+func (p player) Passed() bool {
+	return p.passed
+}
+
+func (p player) GetStoneType() StoneType {
+	return p.stoneType
+}
+
+func (p player) GetTag() string {
+	return p.tag
 }
